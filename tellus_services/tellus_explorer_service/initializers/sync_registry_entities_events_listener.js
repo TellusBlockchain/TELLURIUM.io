93479f1939a040ac222321ea121b7d29a9c4e0da
Yuriy Kashnikov <yuriy@ubikgroup.co>
2019-07-02 18:30:38 +0700

    Fix Infura wss address

--- tellus_services/tellus_explorer_service/initializers/sync_registry_entities_events_listener.js
+++ tellus_services/tellus_explorer_service/initializers/sync_registry_entities_events_listener.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3');
-const web3 = new Web3("wss://ropsten.infura.io/v3/60227cbc12a1491f92b8456ab35e874a");
+const web3 = new Web3("wss://ropsten.infura.io/ws/v3/60227cbc12a1491f92b8456ab35e874a");
 const RegistryEntitiesJSON = require("../../tellus_react/src/contracts/RegistryEntities.json");
 
 let RegistryEntitiesContract = new web3.eth.Contract(
