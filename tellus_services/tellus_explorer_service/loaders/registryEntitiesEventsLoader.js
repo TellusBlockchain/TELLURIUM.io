eb293723f511d16a94cda89aedfc21199627c839
Yuriy Kashnikov <yuriy@ubikgroup.co>
2019-07-02 16:40:29 +0700

    Change provider to Infura of Explorer service

--- tellus_services/tellus_explorer_service/loaders/registryEntitiesEventsLoader.js
+++ tellus_services/tellus_explorer_service/loaders/registryEntitiesEventsLoader.js
@@ -1,10 +1,10 @@
 const Web3 = require('web3');
-const web3 = new Web3("ws://localhost:8545");
+const web3 = new Web3("ws://ropsten.infura.io/v3/60227cbc12a1491f92b8456ab35e874a");
 const RegistryEntitiesJSON = require("../../tellus_react/src/contracts/RegistryEntities.json");
 
 let RegistryEntitiesContract = new web3.eth.Contract(
   RegistryEntitiesJSON.abi,
-  RegistryEntitiesJSON["networks"]["5778"]["address"]
+  RegistryEntitiesJSON["networks"]["3"]["address"]
 );
 
 class registryEntitiesEventsLoader {
