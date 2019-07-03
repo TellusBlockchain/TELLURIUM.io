d96b368d4bd6943eff70b19278fbdca37bb58b01
Yuriy Kashnikov <yuriy@ubikgroup.co>
2019-07-03 13:39:08 +0700

    Make current state build

---
+++ tellus_services/tellus_react/build/precache-manifest.4aa3d7cb6b346a6e686bdaf8377b84b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c173643370a75c2f80c046ae99d68a52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1900b86675049e87538a",
+    "url": "/static/css/main.3ed404f4.chunk.css"
+  },
+  {
+    "revision": "c2aa3eda5882c06f8c6d",
+    "url": "/static/js/2.60c36118.chunk.js"
+  },
+  {
+    "revision": "1900b86675049e87538a",
+    "url": "/static/js/main.ab4477d8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- tellus_services/tellus_react/build/service-worker.js
+++ tellus_services/tellus_react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d3a87df0498645f60d0402e2e2e7e3a.js"
+  "/precache-manifest.4aa3d7cb6b346a6e686bdaf8377b84b2.js"
 );
 
 self.addEventListener('message', (event) => {
