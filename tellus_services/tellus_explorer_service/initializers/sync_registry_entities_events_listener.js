c3bc2b2871094ea46c84fb9991a9602281527e58
Yuriy Kashnikov <yuriy@ubikgroup.co>
2019-07-08 15:35:57 +0700

    Add environment variables

--- tellus_services/tellus_explorer_service/initializers/sync_registry_entities_events_listener.js
+++ tellus_services/tellus_explorer_service/initializers/sync_registry_entities_events_listener.js
@@ -1,10 +1,10 @@
 const Web3 = require('web3');
-const web3 = new Web3(process.env.REACT_APP_WEB3_PROVIDER);
+const web3 = new Web3(process.env.WEB3_PROVIDER);
 const RegistryEntitiesJSON = require("../../tellus_react/src/contracts/RegistryEntities.json");
 
 let RegistryEntitiesContract = new web3.eth.Contract(
   RegistryEntitiesJSON.abi,
-  RegistryEntitiesJSON["networks"][process.env.REACT_APP_ETHEREUM_NETWORK]["address"]
+  RegistryEntitiesJSON["networks"][process.env.ETHEREUM_NETWORK]["address"]
 );
 
 const models = require('../models');
