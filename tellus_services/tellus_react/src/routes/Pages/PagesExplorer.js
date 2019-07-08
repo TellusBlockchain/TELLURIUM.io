492a4b72527945cabeeacacd5acbd33b6c803a2c
Yuriy Kashnikov <yuriy@ubikgroup.co>
2019-07-08 15:34:19 +0700

    Make CSS layout for Explorer page

--- tellus_services/tellus_react/src/routes/Pages/PagesExplorer.js
+++ tellus_services/tellus_react/src/routes/Pages/PagesExplorer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Table, Row, Col } from 'react-bootstrap';
+import { Table, Row, Col, Container,
+         InputGroup, FormControl, Button, Form } from 'react-bootstrap';
 
 class PagesExplorer extends React.Component {
   constructor (props) {
@@ -20,33 +21,59 @@ class PagesExplorer extends React.Component {
 
   render () {
     return (
-      <React.Fragment>
-        <h2>Explorer</h2>
-        <Row>
-          <Col md={{span: 10, offset: 1}}>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>transactionHash</th>
-                  <th>eventName</th>
-                </tr>
-              </thead>
-              <tbody>
-              {
-                this.state.transactions.map((transaction) => {
-                  return (
-                    <tr>
-                      <td><a href={'https://ropsten.etherscan.io/tx/' + transaction.transactionHash}>{transaction.transactionHash}</a></td>
-                      <td>{transaction.eventName}</td>
-                    </tr>
-                  )
-                })
-              }
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-      </React.Fragment>
+      <div style={{ backgroundColor: '#e9e9e9' }}>
+        <Container md={{ span: 10, offset: 1 }}>
+          <Row style={{ paddingTop: '21px' }}>
+            <Col>
+              <InputGroup>
+                <FormControl
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <InputGroup.Append>
+                  <Button variant="outline-secondary">Search</Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </Col>
+            <Col>
+              <Form.Control as="select">
+                <option>AfterRegistryEntityCreate</option>
+                <option>AfterUserCreate</option>
+              </Form.Control>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Table hover className='explorer-table'>
+                <thead>
+                  <tr>
+                    <th>Transaction Hash</th>
+                    <th>Event</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {
+                  this.state.transactions.map((transaction) => {
+                    return (
+                      <tr>
+                        <td>
+                          <a href={'https://ropsten.etherscan.io/tx/' + transaction.transactionHash}
+                              target="_blank"
+                          >
+                            {transaction.transactionHash}
+                          </a>
+                        </td>
+                        <td>{transaction.eventName}</td>
+                      </tr>
+                    )
+                  })
+                }
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
